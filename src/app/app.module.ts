5db7e5352bb79f06b8005d6e199834f8b4250195
tamdna <dongnhatanhtam01@gmail.com>
2020-08-07 20:24:36 +0700

    commit lan 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
 import { NgModule  } from '@angular/core';
-=======
-import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
->>>>>>> 901581493f8dd814da39abe5b56a448aa6e2ba1c
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { ClarityModule } from '@clr/angular';
 import { LayoutModule } from './layout/layout.module';
 import { SideBarComponent } from './side-bar/side-bar.component';
@@ -18,20 +13,11 @@ import { SideBarComponent } from './side-bar/side-bar.component';
   declarations: [
     AppComponent,
     SideBarComponent
-=======
-import { LayoutModule } from './layout/layout.module';
-import { ClarityModule } from '@clr/angular';
-
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> 901581493f8dd814da39abe5b56a448aa6e2ba1c
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-<<<<<<< HEAD
     LayoutModule,
     ClarityModule
     
@@ -39,13 +25,5 @@ import { ClarityModule } from '@clr/angular';
   providers: [],
   bootstrap: [AppComponent]
   // schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
-=======
-    ClarityModule,
-    LayoutModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
->>>>>>> 901581493f8dd814da39abe5b56a448aa6e2ba1c
 })
 export class AppModule { }
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { NgModule,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-=======
-import { NgModule } from '@angular/core';
->>>>>>> 901581493f8dd814da39abe5b56a448aa6e2ba1c
 import { CommonModule } from '@angular/common';
 import { ComponentAComponent } from './component-a/component-a.component';
 import { ComponentBComponent } from './component-b/component-b.component';
@@ -18,12 +14,8 @@ import { ClarityModule } from '@clr/angular/clr-angular.module';
     CommonModule,
     // ClarityModule
   ],
-<<<<<<< HEAD
   exports: [ComponentAComponent, ComponentBComponent, SidenavComponent, ContentAreaComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 
-=======
-  exports: [ComponentAComponent, ComponentBComponent, SidenavComponent, ContentAreaComponent]
->>>>>>> 901581493f8dd814da39abe5b56a448aa6e2ba1c
 })
 export class LayoutModule { }
